fb4d8c565096dc3831bfebc13938f95189b20a0a
MrCoolDogoo <mihneagorcia@gmail.com>
2026-03-04 09:07:30 +0200

    experimental changes

--- script.lua
+++ script.lua
@@ -264,8 +264,8 @@ tab1:Show()
 
 local function tp(pos)
     if array.died then
-        --player.Character.HumanoidRootPart.CFrame = pos
         array.died = false
+        tp(pos)
         return
     end
 
@@ -483,9 +483,9 @@ while true do
                     labels("text", "Moving to: "..npc.Name)
 
                     if myData.Boss[npc.Name] or npc.Parent.Name == "GyakusatsuSpawn" then
-                        tp(npc.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(90),0,0) + Vector3.new(0,myData.DistanceFromBoss,0))
+                        player.HumanoidRootPart.CFrame = (npc.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(90),0,0) + Vector3.new(0,myData.DistanceFromBoss,0))
                     else
-                        tp(npc.HumanoidRootPart.CFrame + npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc)
+                        player.HumanoidRootPart.CFrame = (npc.HumanoidRootPart.CFrame + npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc)
                     end
 
                     labels("text", "Killing: "..npc.Name)
